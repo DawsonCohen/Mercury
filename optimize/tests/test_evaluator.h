461fe3c4e3a9bd03b17ed028834e646a023ea45a
Dawson Cohen <dawson@the-cohens.com>
2023-10-30 19:56:17 -0400

    added encode/decode check

--- optimize/tests/test_evaluator.h
+++ optimize/tests/test_evaluator.h
@@ -32,6 +32,8 @@ std::vector<float> runEvaluator(std::vector<NNRobot> evalBuf) {
 }
 
 int TestEvaluator() {
+    srand(50);
+
 	OptimizerConfig config;
 	config.evaluator.pop_size = ROBO_COUNT;
 	std::vector<NNRobot> population(ROBO_COUNT);
@@ -54,7 +56,7 @@ int TestEvaluator() {
 	reset_fitness = runEvaluator(evalBuf);
 	
 	int successflag = 0;
-	for(uint i = 0; i < population.size(); i++) {
+	for(uint i = 0; i < ROBO_COUNT; i++) {
 		printf("%f vs %f", og_fitness[i], reset_fitness[i]);
 		if(abs(og_fitness[i] - reset_fitness[i]) > EPS) {
 			printf(" FAILED");
@@ -69,13 +71,28 @@ int TestEvaluator() {
 	printf("DECODED EVALUATION COMPARISON\n");
 	std::vector<NNRobot> decode_evalBuf(ROBO_COUNT);
 	for(uint i = 0; i < evalBuf.size(); i++) {
+		NNRobot R = evalBuf[i];
+		NNRobot Rdecoded;
 		std::string encoding = evalBuf[i].Encode();
 		util::WriteCSV("test_NNBot_" + std::to_string(i) + ".csv","./z_results/",encoding);
 
-		NNRobot Rcopy;
 		std::string filepath = "./z_reslts/test_NNBot_" + std::to_string(i) + ".csv";
-		Rcopy.Decode(filepath);
-		decode_evalBuf[i] = Rcopy;	
+		Rdecoded.Decode(filepath);
+		decode_evalBuf[i] = Rdecoded;
+
+		for(size_t i = 0; i < Rdecoded.masses.size(); i++) {
+			if((R.masses[i].pos - Rdecoded.masses[i].pos).norm() > EPS) {
+				Eigen::Vector3f pos1 = R.masses[i].pos;
+				Eigen::Vector3f pos2 = Rdecoded.masses[i].pos;
+				printf("(%f,%f,%f) vs (%f,%f,%f)\n",pos1[0],pos1[1],pos1[2],pos2[0],pos2[1],pos2[2]);
+				successflag += 1;
+			}
+			if(R.masses[i].material != Rdecoded.masses[i].material) {
+				printf("%lu: %u vs %u\n",i, R.masses[i].material.encoding, Rdecoded.masses[i].material.encoding);
+				successflag += 1;
+			}
+		}
+
 	}
 	decode_fitness = runEvaluator(decode_evalBuf);
 	for(uint i = 0; i < ROBO_COUNT; i++) {
