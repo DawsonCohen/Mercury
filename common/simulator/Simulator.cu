eb469d3fa4cd95114527438ef7bb70e25ff5cb0d
AmrEl-Azizi <elaziziamr@gmail.com>
2023-11-28 11:27:27 -0500

    Moved all of sim.cu to one massive file to fix things somehow

--- common/simulator/Simulator.cu
+++ common/simulator/Simulator.cu
@@ -1,11 +1,368 @@
 #include "Simulator.h"
-#include "sim_kernel.cu"
-#include "devo_kernel.cu"
 #include <math.h>
 #include <algorithm>
 #include <random>
 #include <map>
 #include "util.h"
+#include "vec_math.cuh"
+#include "material.h"
+#include <curand_kernel.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdio.h>
+#include "environment.h"
+
+struct SimOptions {
+    float dt;
+    uint massesPerBlock;
+    uint springsPerBlock;
+    uint facesPerBlock;
+    uint cellsPerBlock;
+    uint boundaryMassesPerBlock;
+    uint maxMasses;
+    uint maxSprings;
+    uint maxFaces;
+    uint maxCells;
+    uint compositeCount;
+    short shiftskip;
+    float drag;
+    float damping;
+    float relaxation;
+    float s;
+};
+
+struct DevoOptions {
+    uint maxReplacedSprings;
+    uint maxSprings;
+    uint springsPerElement;
+    uint massesPerElement;
+    uint replacedSpringsPerElement;
+    uint compositeCount;
+};
+
+__constant__ float4 compositeMats_id[COMPOSITE_COUNT];
+__constant__ SimOptions cSimOpt;
+__constant__ float4 compositeMats_encoding[COMPOSITE_COUNT];
+__constant__ DevoOptions cDevoOpt;
+
+
+
+#define EPS (float) 1e-12
+#define MAX_FORCE (float) 200000
+
+__global__ inline
+void surfaceDragForce(float4 *__restrict__ pos, float4 *__restrict__ newPos,
+                 float4 *__restrict__ vel, ushort4 *__restrict__ faces) {
+	extern __shared__ float3 s[];
+	float3  *s_pos = s;
+	float3  *s_vel = (float3*) &s_pos[cSimOpt.boundaryMassesPerBlock];
+	float3  *s_force = (float3*) &s_vel[cSimOpt.boundaryMassesPerBlock];
+	
+	uint massOffset   = blockIdx.x * cSimOpt.massesPerBlock;
+	uint faceOffset   = blockIdx.x * cSimOpt.facesPerBlock;
+	uint i;
+
+	int tid    = threadIdx.x;
+	int stride = blockDim.x;
+	
+	// Initialize and compute environment forces
+	float4 pos4, vel4;
+	for(i = tid; i < cSimOpt.boundaryMassesPerBlock && (i+massOffset) < cSimOpt.maxMasses; i+=stride) {
+		pos4 = __ldg(&pos[i+massOffset]);
+		vel4 = __ldg(&vel[i+massOffset]);
+		s_pos[i] = {pos4.x,pos4.y,pos4.z};
+		s_vel[i] = {vel4.x,vel4.y,vel4.z};
+		s_force[i] = {0.0f, 0.0f, 0.0f};
+	}
+	
+	float rho = cSimOpt.drag, area;
+	ushort4 face;
+	float3  x0, x1, x2,
+	        v0, v1, v2,
+			v, normal, force;
+	for(i = tid; i < cSimOpt.facesPerBlock && (i+faceOffset) < cSimOpt.maxFaces; i+=stride) {
+		// Drag Force: 0.5*rho*A*((Cd - Cl)*dot(v,n)*v + Cl*dot(v,v)*n)
+		face = __ldg(&faces[i+faceOffset]);
+		if(face.x == face.y || face.x == face.z || face.y == face.z)
+			continue;
+
+		x0 = s_pos[face.x];
+		x1 = s_pos[face.y];
+		x2 = s_pos[face.z];
+		v0 = s_vel[face.x];
+		v1 = s_vel[face.y];
+		v2 = s_vel[face.z];
+		
+		v = (v0 + v1 + v2) / 3.0f;
+		normal = cross((x1 - x0), (x2-x0));
+		area = norm3df(normal.x,normal.y,normal.z);
+		normal = normal / (area + EPS);
+		normal = dot(normal, v) > 0.0f ? normal : -normal;
+		force = -0.5*rho*area*(dot(v,normal)*v + 0.2*dot(v,v)*normal);
+		force = force / 3.0f; // allocate forces evenly amongst masses
+		
+		atomicAdd(&(s_force[face.x].x), force.x);
+		atomicAdd(&(s_force[face.x].y), force.y);
+		atomicAdd(&(s_force[face.x].z), force.z);
+
+		atomicAdd(&(s_force[face.y].x), force.x);
+		atomicAdd(&(s_force[face.y].y), force.y);
+		atomicAdd(&(s_force[face.y].z), force.z);
+
+		atomicAdd(&(s_force[face.z].x), force.x);
+		atomicAdd(&(s_force[face.z].y), force.y);
+		atomicAdd(&(s_force[face.z].z), force.z);
+	}
+
+	for(i = tid; i < cSimOpt.boundaryMassesPerBlock && (i+massOffset) < cSimOpt.maxMasses; i+=stride) {
+		x0 = s_pos[i];
+		v0 = s_vel[i];
+		force = s_force[i];
+		newPos[i+massOffset].x = x0.x + v0.x*cSimOpt.dt + force.x*cSimOpt.dt*cSimOpt.dt;
+		newPos[i+massOffset].y = x0.y + v0.y*cSimOpt.dt + force.y*cSimOpt.dt*cSimOpt.dt;
+		newPos[i+massOffset].z = x0.z + v0.z*cSimOpt.dt + force.z*cSimOpt.dt*cSimOpt.dt;
+	}
+}
+
+__global__ inline
+void preSolve(float4 *__restrict__ pos, float4 *__restrict__ newPos,
+                 float4 *__restrict__ vel) {
+	int stride = blockDim.x * gridDim.x;
+	float4 velocity;
+
+	for(uint i = blockIdx.x * blockDim.x + threadIdx.x;
+		i < cSimOpt.maxMasses; i+=stride) {
+		//Force due to drag = - (1/2 * rho * |v|^2 * A * Cd) * v / |v| (Assume A and Cd are 1)
+		velocity = __ldg(&vel[i]);
+		newPos[i] = __ldg(&pos[i]) + velocity*cSimOpt.dt;
+	}
+}
+
+
+/*
+	Exended Positon Based Dynamics
+	Computes lagrangian (force) for each distance constraint (spring)
+
+	mat: float4 describing spring material
+		x - k		stiffness
+		y - dL0 	maximum percent change
+		z - omega	frequency of oscillation
+		w - phi		phase
+*/
+__global__ inline
+void solveDistance(float4 *__restrict__ newPos, ushort2 *__restrict__ pairs, 
+				float * __restrict__ stresses, uint8_t *__restrict__ matIds, float *__restrict__ Lbars,
+				float time, uint step, bool integrateForce)
+{
+	extern __shared__ float3 s[];
+	float3  *s_pos = s;
+	float3  *s_dp = (float3*) &s_pos[cSimOpt.massesPerBlock];
+	
+	uint massOffset   = blockIdx.x * cSimOpt.massesPerBlock;
+	uint springOffset = blockIdx.x * cSimOpt.springsPerBlock;
+	uint i;
+
+	int tid    = threadIdx.x;
+	int stride = blockDim.x;
+	
+	// Initialize and compute environment forces
+	float4 pos4;
+	for(i = tid; i < cSimOpt.massesPerBlock && (i+massOffset) < cSimOpt.maxMasses; i+=stride) {
+		pos4 = __ldg(&newPos[i+massOffset]);
+		s_pos[i] = {pos4.x,pos4.y,pos4.z};
+		s_dp[i] = {0.0f, 0.0f, 0.0f};
+	}
+
+	__syncthreads();
+
+	float4	 mat;
+	uint8_t  matId;
+
+	float3	 pos0, pos1;
+	float	 Lbar,
+			 C, alpha,
+			 lambda;
+	ushort	 v0, v1;
+	ushort2	 pair;
+
+	float3	distance, n;
+
+	float	relative_change,
+			rest_length,
+			d, K;
+	float3  dp;
+	
+	for(i = tid; i < cSimOpt.springsPerBlock && (i+springOffset) < cSimOpt.maxSprings; i+=stride) {
+		matId = __ldg(&matIds[i+springOffset]);
+		if(matId == materials::air.id) continue;
+
+		pair = __ldg(&pairs[i+springOffset]);
+		Lbar = __ldg(&Lbars[i+springOffset]);
+		v0 = pair.x; v1 = pair.y;
+		pos0 = s_pos[v0];
+		pos1 = s_pos[v1];
+
+		mat = compositeMats_id[ matId ];
+		alpha = 1.0f / mat.x / cSimOpt.dt / cSimOpt.dt;
+		// rest_length = mean_length * (1 + relative_change);
+		relative_change = mat.y * sinf(mat.z*time+mat.w);
+		rest_length = __fmaf_rn(Lbar, relative_change, Lbar);
+		
+		K = 2.0f + alpha;
+		distance = pos0-pos1;
+		d = l2norm(distance);
+		n = distance / (d + EPS);
+		
+		C = d-rest_length;
+		lambda = -(C) / (K);
+		dp = lambda * n;
+
+		if(integrateForce) stresses[i+springOffset] += lambda / Lbar;
+
+		atomicAdd(&(s_dp[v0].x), dp.x);
+		atomicAdd(&(s_dp[v0].y), dp.y);
+		atomicAdd(&(s_dp[v0].z), dp.z);
+
+		atomicAdd(&(s_dp[v1].x), -dp.x);
+		atomicAdd(&(s_dp[v1].y), -dp.y);
+		atomicAdd(&(s_dp[v1].z), -dp.z);
+	}
+	__syncthreads();
+
+	for(i = tid; i < cSimOpt.massesPerBlock && (i+massOffset) < cSimOpt.maxMasses; i+=stride) {
+		pos4 =__ldg(&newPos[i+massOffset]);
+		pos4.x += s_dp[i].x;
+		pos4.y += s_dp[i].y;
+		pos4.z += s_dp[i].z;
+		newPos[i+massOffset] = pos4;
+	}
+}
+
+__global__
+inline void update(float4 *__restrict__ pos, float4 *__restrict__ newPos, float4 *__restrict__ vel) {
+	// Calculate and store new mass states
+	int stride = blockDim.x * gridDim.x;
+
+	float4 newPos4, pos4;
+	for(uint i = blockIdx.x * blockDim.x + threadIdx.x; i < cSimOpt.maxMasses; i+=stride) {
+		pos4 = __ldg(&pos[i]);
+		newPos4 = __ldg(&newPos[i]);
+		pos[i] = newPos4;
+		vel[i].x = 0.99*(newPos4.x - pos4.x) / cSimOpt.dt;
+		vel[i].y = 0.99*(newPos4.y - pos4.y) / cSimOpt.dt;
+		vel[i].z = 0.99*(newPos4.z - pos4.z) / cSimOpt.dt;
+	}
+}
+
+__global__ void
+inline randomIntegerPairKernel(int N, ushort2* results, int min_value, int max_value, unsigned int seed) {
+
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x;
+    
+    curandState state;
+    curand_init(seed, tid, 0, &state);
+    
+    for(uint i = tid; i < N; i += stride) {
+        ushort random_integer_x  = curand(&state) % (max_value - min_value + 1) + min_value;
+        ushort random_integer_y  = curand(&state) % (max_value - min_value + 1) + min_value;
+        while(random_integer_x == random_integer_y) {
+            random_integer_y  = curand(&state) % (max_value - min_value + 1) + min_value;
+        }
+        results[i].x = random_integer_x;
+        results[i].y = random_integer_y;
+    }
+}
+
+__global__ void
+inline replaceSprings(
+    ushort2 *__restrict__ pairs,
+    uint32_t *__restrict__ massMatEncodings, 
+    float4 *__restrict__ massPos,
+    float *__restrict__ Lbars,
+    uint32_t *__restrict__ springMatEncodings,
+    uint *__restrict__ sortedSpringIds,
+    ushort2* newPairs,
+    float time
+) {
+	int tid    = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x;
+
+    uint    elementId,
+            massOffset,
+            sortedSpringId,
+            springId;
+	ushort2	newPair;
+	ushort	left, right;
+    float4  posLeft, posRight;
+    float4  newMat;
+    float3  posDiff;
+    float   rest_length,
+            relative_change;
+	uint32_t	matEncodingLeft, matEncodingRight, newMatEncoding;
+    uint i, j;
+    uint idx[2] = {0,0},matIdx,
+        count,bitmask;
+
+	for(i = tid; 
+        i < cDevoOpt.maxReplacedSprings && (i / cDevoOpt.replacedSpringsPerElement) * cDevoOpt.springsPerElement + i < cDevoOpt.maxSprings; 
+        i+=stride)
+    {
+        elementId = (i / cDevoOpt.replacedSpringsPerElement);
+        massOffset = elementId * cDevoOpt.massesPerElement;
+        sortedSpringId = elementId * cDevoOpt.springsPerElement + i;
+
+        newPair = __ldg(&newPairs[i]);
+        springId = __ldg(&sortedSpringIds[sortedSpringId]);
+
+		pairs[springId] = newPair;
+        
+        left  = newPair.x;
+		right = newPair.y;
+
+        matEncodingLeft = massMatEncodings[left + massOffset];
+        matEncodingRight = massMatEncodings[right + massOffset];
+
+
+        posLeft =  massPos[left+massOffset]; 
+        posRight =  massPos[right+massOffset];
+
+        posDiff = {
+            posLeft.x - posRight.x,
+            posLeft.y - posRight.y,
+            posLeft.z - posRight.z
+        };
+
+        newMatEncoding = matEncodingLeft | matEncodingRight;
+
+        count = 0;
+        bitmask = 0x01u;
+        for(j = 0; j < COMPOSITE_COUNT; j++) {
+            if(newMatEncoding & bitmask) {
+                idx[count] = j;
+                count++;
+				if(j == 0 || count == 2) break;
+            }
+            bitmask <<= 1;
+        }
+        if(idx[0] == 0) {
+            matIdx = 0;
+        } else if(idx[1] == 0) {
+            matIdx = 1 + idx[0]*(idx[0]-1)/2;
+        } else {
+            matIdx = 1 + idx[1]*(idx[1]-1)/2 + idx[0];
+        }
+
+        newMat = compositeMats_encoding[matIdx];
+
+        rest_length = l2norm(posDiff);
+        relative_change = newMat.y * sinf(newMat.z * time + newMat.w);
+
+        Lbars[springId] = rest_length / (1+relative_change);
+        springMatEncodings[springId] = newMatEncoding;
+
+    }
+}
 
 #include <cub/device/device_segmented_radix_sort.cuh>
 
